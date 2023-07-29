23dd0d6d815f36801d53dc61f37f62bf009a1ea7
Kateryna Verveda <katushka211@gmail.com>
2023-07-29 20:08:59 +0200

    add styles and settings for slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import Swiper from 'swiper/swiper-bundle.min.mjs';
 
-const swiperMain = new Swiper('.slider_main', {});
+const swiperMain = new Swiper('.slider_main', {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60,
+    },
+  },
+});
