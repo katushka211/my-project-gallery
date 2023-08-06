c5776b0e00857a4c9dcd383aa89ac088dd1b5b4d
Kateryna Verveda <katushka211@gmail.com>
2023-08-07 00:26:11 +0200

    add effect parallax

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+import 'swiper/swiper-bundle.min.css';
 import Swiper from 'swiper/swiper-bundle.min.mjs';
 
-const swiperMain = new Swiper('.slider_main', {
-  freeMode: true,
-  centeredSlides: true,
-  mousewheel: true,
-  breakpoints: {
-    0: {
-      slidesPerView: 2.5,
-      spaceBetween: 20,
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperMain = new Swiper('.slider_main', {
+    freeMode: true,
+    centeredSlides: true,
+    mousewheel: true,
+    parallax: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      680: {
+        slidesPerView: 3.5,
+        spaceBetween: 60,
+      },
     },
-    680: {
-      slidesPerView: 3.5,
-      spaceBetween: 60,
-    },
-  },
+  });
 });
