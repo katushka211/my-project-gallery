e7b1ba500c12ce765c425677d073422d4b186cbe
Kateryna Verveda <katushka211@gmail.com>
2023-09-06 09:38:18 +0200

    add styles for slider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'swiper/swiper-bundle.min.css';
 import Swiper from 'swiper/swiper-bundle.min.mjs';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const swiperMain = new Swiper('.slider_main', {
+  const sliderMain = new Swiper('.slider_main', {
     freeMode: true,
     centeredSlides: true,
     mousewheel: true,
@@ -17,5 +17,20 @@ document.addEventListener('DOMContentLoaded', () => {
         spaceBetween: 60,
       },
     },
+  });
+  const sliderBg = new Swiper('.slider_bg', {
+    centeredSlides: true,
+    parallax: true,
+    slidesPerView: 3.5,
+    spaceBetween: 60,
+  });
+  sliderMain.controller.control = sliderBg;
+});
+
+const sliderItems = document.querySelectorAll('.slider__item');
+
+sliderItems.forEach(sliderItem => {
+  sliderItem.addEventListener('click', event => {
+    sliderItem.classList.toggle('opened');
   });
 });
