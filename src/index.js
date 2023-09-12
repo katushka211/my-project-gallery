fa7cc4f7dd455d4fa160d4361e5e02ba253cb4ab
Kateryna Verveda <katushka211@gmail.com>
2023-09-12 12:42:04 +0200

    add styles, update swiper

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 import 'swiper/swiper-bundle.min.css';
 import Swiper from 'swiper/swiper-bundle.min.mjs';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sliderMain = new Swiper('.slider_main', {
-    freeMode: true,
-    centeredSlides: true,
-    mousewheel: true,
-    parallax: true,
-    breakpoints: {
-      0: {
-        slidesPerView: 2.5,
-        spaceBetween: 20,
-      },
-      680: {
-        slidesPerView: 3.5,
-        spaceBetween: 60,
-      },
+const sliderMain = new Swiper('.slider_main', {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
     },
-  });
-  const sliderBg = new Swiper('.slider_bg', {
-    centeredSlides: true,
-    parallax: true,
-    slidesPerView: 3.5,
-    spaceBetween: 60,
-  });
-  sliderMain.controller.control = sliderBg;
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60,
+    },
+  },
 });
+const sliderBg = new Swiper('.slider_bg', {
+  centeredSlides: true,
+  parallax: true,
+  slidesPerView: 3.5,
+  spaceBetween: 60,
+});
+sliderBg.controller.control = sliderMain;
 
 const sliderItems = document.querySelectorAll('.slider__item');
 
-sliderItems.forEach(sliderItem => {
-  sliderItem.addEventListener('click', event => {
-    sliderItem.classList.toggle('opened');
+sliderItems.forEach(item => {
+  item.addEventListener('click', event => {
+    item.classList.toggle('opened');
   });
 });
